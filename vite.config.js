1f6e8d63c7b684025ce535d0c765baccedcc4301
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-08 20:07:01 +0500

    add build config

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "movie.html": resolve(__dirname, "movie.html"),
+        "search.html": resolve(__dirname, "search.html"),
+      },
+    },
+  },
+});
