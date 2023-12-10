020bb0022ecf6fb267b043019fa83217533c172f
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-10 21:42:42 +0500

    refactor

--- src/js/modules/pagination.js
+++ src/js/modules/pagination.js
@@ -2,58 +2,40 @@ export const renderPagination = (page, total, el, query, texts, url = "/?") => {
   el.innerHTML = "";
   const prevPage = page === 1 ? total : page - 1;
 
-  console.log(total);
-
   const nextPage = page === total ? 1 : page + 1;
   const slice1 = page === 1 ? page - 1 : page === 2 ? page - 2 : page - 3;
 
-  const pages = Array.from({ length: total }, (_, idx) => idx + 1);
+  const pages = Array.from({ length: total }, (_, i) => i + 1);
   const slicedPages = pages.slice(slice1, page + 2);
 
   let html = `<li class="page-item">
                 <a class="page-link" href="${url}${query}=${prevPage}">${texts.prev}</a>
               </li>`;
 
-  // 1th page
+  // 1th page and dots
   if (page > 3) {
-    html += `
-    <li class="page-item">
-      <a class="page-link" href="${url}${query}=1">1</a>
-    </li>
-    `;
-  }
-
-  // 1st dots
-  if (page > 3) {
-    html += `
-    <li class="page-item">
-      <a class="page-link">...</a>
-    </li>
+    html += `<li class="page-item">
+              <a class="page-link" href="${url}${query}=1">1</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link">...</a>
+            </li>
     `;
   }
 
   // forEach pages
   slicedPages.forEach(pg => {
-    const isActive = pg === page;
-
-    html += `
-            <li class="page-item ${isActive ? "active" : ""}">
+    html += `<li class="page-item ${pg === page ? "active" : ""}">
               <a class="page-link" href="${url}${query}=${pg}">${pg}</a>
             </li>`;
   });
 
-  // 2nd dots
+  // dots and last page
   if (page < total - 2) {
     html += `
       <li class="page-item">
         <a class="page-link">...</a>
       </li>
-    `;
-  }
-
-  // last page
-  if (page < total - 2) {
-    html += `
       <li class="page-item">
         <a class="page-link" href="${url}${query}=${total}">${total}</a>
       </li>
