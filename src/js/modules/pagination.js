5e9cf97dc81e2686a60b1efe84da1b6a1adaa56c
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-10 22:16:23 +0500

    add changes

--- src/js/modules/pagination.js
+++ src/js/modules/pagination.js
@@ -5,37 +5,53 @@ export const renderPagination = (page, total, el, query, texts, url = "/?") => {
   const nextPage = page === total ? 1 : page + 1;
   const slice1 = page === 1 ? page - 1 : page === 2 ? page - 2 : page - 3;
 
-  const pages = Array.from({ length: total }, (_, i) => i + 1);
+  const pages = Array.from({ length: total }, (_, idx) => idx + 1);
   const slicedPages = pages.slice(slice1, page + 2);
 
   let html = `<li class="page-item">
                 <a class="page-link" href="${url}${query}=${prevPage}">${texts.prev}</a>
               </li>`;
 
-  // 1th page and dots
+  // 1th page
   if (page > 3) {
-    html += `<li class="page-item">
-              <a class="page-link" href="${url}${query}=1">1</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link">...</a>
-            </li>
+    html += `
+    <li class="page-item">
+      <a class="page-link" href="${url}${query}=1">1</a>
+    </li>
+    `;
+  }
+
+  // 1st dots
+  if (page > 3) {
+    html += `
+    <li class="page-item">
+      <a class="page-link">...</a>
+    </li>
     `;
   }
 
   // forEach pages
   slicedPages.forEach(pg => {
-    html += `<li class="page-item ${pg === page ? "active" : ""}">
+    const isActive = pg === page;
+
+    html += `
+            <li class="page-item ${isActive ? "active" : ""}">
               <a class="page-link" href="${url}${query}=${pg}">${pg}</a>
             </li>`;
   });
 
-  // dots and last page
+  // 2nd dots
   if (page < total - 2) {
     html += `
       <li class="page-item">
         <a class="page-link">...</a>
       </li>
+    `;
+  }
+
+  // last page
+  if (page < total - 2) {
+    html += `
       <li class="page-item">
         <a class="page-link" href="${url}${query}=${total}">${total}</a>
       </li>
